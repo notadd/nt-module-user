1adc42445873c38986caad8a035f9a15b9b87b18
dzzzzzy <insistyao_0522@163.com>
2018-08-02 15:08:39 +0800

    chore(resolver): add simple user resolver

---
+++ src/resolvers/user.resolver.ts
@@ -0,0 +1,24 @@
+import { Inject } from '@nestjs/common';
+import { Mutation, Query, Resolver } from '@nestjs/graphql';
+
+import { AuthService } from '../auth';
+import { UserService } from '../services/user.service';
+
+@Resolver()
+export class UserResolver {
+    constructor(
+        @Inject(AuthService) private readonly authService: AuthService,
+        @Inject(UserService) private readonly userService: UserService
+    ) { }
+
+    @Query('login')
+    async login(req, body: { username: string, password: string }) {
+        const data = await this.userService.login(body.username, body.password);
+        return { code: 200, message: '登录成功', data };
+    }
+
+    @Mutation('register')
+    async register(req, body: { username: string, pasword: string, info: any }) {
+        // TODO: 注册时，用户信息是不确定的？？？
+    }
+}

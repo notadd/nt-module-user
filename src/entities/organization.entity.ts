9d3728d4a19ae01206fda7367b436f588c089d9e
iyangsheng <1648569746@qq.com>
2018-08-03 18:33:13 +0800

    chore(entity): add decorators

--- src/entities/organization.entity.ts
+++ src/entities/organization.entity.ts
@@ -1,15 +1,26 @@
-import { Entity, Tree, TreeParent, TreeChildren } from 'typeorm';
+import { Entity, Tree, TreeParent, TreeChildren, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Tree('closure-table')
 @Entity('organization')
 export class Organization {
+    @PrimaryGeneratedColumn()
     id: number;
 
+    /**
+     * 组织名称
+     */
+    @Column()
     name: string;
 
+    /**
+     * 父组织
+     */
     @TreeParent()
     parent: Organization;
 
+    /**
+     * 子组织
+     */
     @TreeChildren()
     children: Organization[];
 }

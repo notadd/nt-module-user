7348b055a1e73312407b68be084eab292f5ab6bb
iyangsheng <1648569746@qq.com>
2018-08-03 17:15:40 +0800

    chore(role): add setPermissions logic

--- src/services/role.service.ts
+++ src/services/role.service.ts
@@ -2,11 +2,13 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable, Inject, HttpException } from '@nestjs/common';
 import { Role } from '../entities/role.entity';
 import { Repository } from 'typeorm';
+import { Permission } from '../entities';
 
 @Injectable()
 export class RoleService {
     constructor (
-        @InjectRepository(Role) private readonly roleRep: Repository<Role>
+        @InjectRepository(Role) private readonly roleRep: Repository<Role>,
+        @InjectRepository(Permission) private readonly permissionRep: Repository<Permission>
     ) {}
 
     /**
@@ -66,6 +68,36 @@ export class RoleService {
             await this.roleRep.delete(role);
         } catch (err) {
             throw new HttpException(`数据库错误：${err.toString()}`, 401);
+        }
+    }
+
+    /**
+     * 给角色设置权限
+     * @param id 角色ID
+     * @param permissionIds 权限ID数组
+     */
+    async setPermissions(id: number, permissionIds: number[]) {
+        const role = await this.roleRep.findOne(id);
+        if (!role) {
+            new HttpException(`id为${id}的角色不存在`, 404);
+        }
+
+        const permissionArr = await this.permissionRep.findByIds(permissionIds);
+        permissionIds.forEach(permissionId => {
+            const exist: Permission|undefined = permissionArr.find(permission => {
+                return permission.id === permissionId;
+            });
+
+            if (!exist) {
+                throw new HttpException(`id为${permissionId}的权限不存在`, 404);
+            }
+        });
+
+        role.permissions = permissionArr;
+        try {
+            await this.roleRep.save(role);
+        } catch (error) {
+            throw new HttpException(`数据库错误：${error.toString()}`, 401);
         }
     }
 }

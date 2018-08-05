77571b04b04b98bd57e68c01943db336f74ea1f0
iyangsheng <1648569746@qq.com>
2018-08-05 23:58:54 +0800

    chore(resolver): add graphql decorator

--- src/resolvers/organization.resolver.ts
+++ src/resolvers/organization.resolver.ts
@@ -1,5 +1,5 @@
 import { OrganizationService } from './../services/organization.service';
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation } from '@nestjs/graphql';
 
 @Resolver()
 export class OrganizationResolver {
@@ -7,34 +7,40 @@ export class OrganizationResolver {
         private readonly organizationService: OrganizationService
     ) {}
 
+    @Query('getRoots')
     async getRoots() {
         const roots = await this.organizationService.getRoots();
         return {code: 200, message: '获取成功', data: roots};
     }
 
+    @Query('getAll')
     async getAll() {
         const organizationArr = await this.organizationService.getAll();
         return {code: 200, message: '获取成功', data: organizationArr};
     }
 
+    @Query('findChildren')
     async findChildren(req, body: {id: number}) {
         const {id} = body;
         const organizationArr = await this.organizationService.findChildren(id);
         return {code: 200, message: '获取成功', data: organizationArr};
     }
 
+    @Mutation('createOrganization')
     async createOrganization(req, body: {name: string, parentId: number}) {
         const {name, parentId} = body;
         await this.organizationService.createOrganization(name, parentId);
         return {code: 200, message: '添加成功'};
     }
 
+    @Mutation('updateOrganization')
     async updateOrganization(req, body: {id: number, name: string, parentId: number}) {
         const {id, name, parentId} = body;
         await this.organizationService.updateOrganization(id, name, parentId);
         return {code: 200, message: '更新成功'};
     }
 
+    @Mutation('deleteOrganization')
     async deleteOrganization(req, body: {id: number}) {
         const {id} = body;
         await this.organizationService.deleteOrganization(id);

0b86a3aa04baf660f4e3ae27fa5c2874b0faec8b
dzzzzzy <insistyao_0522@163.com>
2018-08-07 19:25:09 +0800

    feat(util): add check password method

--- src/utils/crypto.util.ts
+++ src/utils/crypto.util.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
 
 /**
- * 加密工具
+ * 加解密工具
  */
 @Injectable()
 export class CryptoUtil {
@@ -14,5 +14,15 @@ export class CryptoUtil {
      */
     async encryptPassword(password: string): Promise<string> {
         return bcrypt.hash(password, bcrypt.genSaltSync());
+    }
+
+    /**
+     * 检查密码是否正确
+     *
+     * @param password 明文密码
+     * @param passwordHash 密文密码
+     */
+    async checkPassword(password: string, passwordHash: string): Promise<boolean> {
+        return bcrypt.compare(password, passwordHash);
     }
 }

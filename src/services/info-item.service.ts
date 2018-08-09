e320b90727415c4b6540a8554db943c1240dbcf2
dzzzzzy <insistyao_0522@163.com>
2018-08-09 17:52:24 +0800

    feat(service): add info item service logic

---
+++ src/services/info-item.service.ts
@@ -0,0 +1,54 @@
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { InfoItem } from '../entities/info-item.entity';
+
+@Injectable()
+export class InfoItemService {
+    constructor(
+        @InjectRepository(InfoItem) private readonly infoItemRepo: Repository<InfoItem>
+    ) { }
+
+    /**
+     * 创建信息项
+     *
+     * @param infoItem 信息项信息
+     */
+    async create(infoItem: InfoItem) {
+        this.infoItemRepo.save(this.infoItemRepo.create(infoItem));
+    }
+
+    /**
+     * 删除信息项
+     *
+     * @param id 信息项ID
+     */
+    async delete(id: number) {
+        this.infoItemRepo.delete(id);
+    }
+
+    /**
+     * 更新信息项的指定信息
+     *
+     * @param id 信息项ID
+     * @param name 信息项名称
+     * @param label 信息项标签
+     * @param description 信息项描述
+     * @param type 信息项类型
+     */
+    async update(id: number, name: string, label: string, description: string, type: string) {
+        const nameExist = await this.infoItemRepo.findOne({ where: { name } });
+        if (nameExist) {
+            throw new HttpException(`信息项名称：${name} 已存在`, 409);
+        }
+        this.infoItemRepo.update(id, { name, label, description, type });
+    }
+
+    /**
+     * 查询所有信息项
+     */
+    async findAll() {
+        return this.infoItemRepo.find();
+    }
+}

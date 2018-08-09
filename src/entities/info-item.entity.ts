7417af00fc958c33a287545f7cf386739e6f7f8a
dzzzzzy <insistyao_0522@163.com>
2018-08-09 17:51:25 +0800

    feat(entity): add info item entity

---
+++ src/entities/info-item.entity.ts
@@ -0,0 +1,54 @@
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+import { InfoGroup } from './info.group.entity';
+import { UserInfo } from './user-info.entity';
+
+/**
+ * 用户信息项表
+ */
+@Entity('info_item')
+export class InfoItem {
+    /**
+     * 自增ID
+     */
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    /**
+     * 信息项名称
+     */
+    @Column({
+        unique: true
+    })
+    name: string;
+
+    /**
+     * 信息项标签
+     */
+    @Column()
+    label: string;
+
+    /**
+     * 信息项描述
+     */
+    @Column()
+    description: string;
+
+    /**
+     * 信息项类型
+     */
+    @Column()
+    type: string;
+
+    /**
+     * 用户信息项对应的值
+     */
+    @ManyToMany(type => UserInfo, userInfo => userInfo.infoItems)
+    userInfos: UserInfo[];
+
+    /**
+     * 信息项所属的信息组
+     */
+    @ManyToMany(type => InfoGroup, infoGroup => infoGroup.infoItems)
+    infoGroups: InfoGroup[];
+}

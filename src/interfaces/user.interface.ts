2c8568fac69d4e56d5a5ce24fedf590d67c211b2
dzzzzzy <insistyao_0522@163.com>
2018-08-10 18:38:46 +0800

    chore(interface): update user interface

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,3 +1,57 @@
-export interface UserUpdateInput {
+export interface CreateUserInput {
+    /**
+     * 用户名
+     */
+    username: string;
+    /**
+     * 用户邮箱
+     */
+    email?: string;
+    /**
+     * 用户手机号
+     */
+    mobile?: string;
+    /**
+     * 登录密码
+     */
     password: string;
+    /**
+     * 信息项键值对，key是信息项的ID(infoItem.id)，值是信息项的值(userInfo.value)
+     */
+    infoKVs: { key: number, value: string }[];
+    /**
+     * 拥有的角色ID数组
+     */
+    roleIds: number[];
+    /**
+     * 所属的组织ID数组
+     */
+    organizationIds?: number[];
+}
+
+export interface UpdateUserInput {
+    /**
+     * 用户邮箱
+     */
+    email?: string;
+    /**
+     * 用户手机号
+     */
+    mobile?: string;
+    /**
+     * 登录密码
+     */
+    password?: string;
+    /**
+     * 信息项键值对
+     */
+    infoKVs?: { key: number, value: string }[];
+    /**
+     * 拥有的角色ID数组
+     */
+    roleIds?: number[];
+    /**
+     * 所属的组织ID数组
+     */
+    organizationIds?: number[];
 }

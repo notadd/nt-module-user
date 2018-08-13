97e65c159cf6e98c07234b5754dddb838beb5ac5
dzzzzzy <insistyao_0522@163.com>
2018-08-13 18:18:20 +0800

    chore(decorator): remove property

--- src/decorators/permission.decorator.ts
+++ src/decorators/permission.decorator.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 
 export const PERMISSION_DEFINITION = '__permission_definition__';
 
-export function Permission(options: { name: string, identify: string, action: 'create' | 'delete' | 'update' | 'find', personal?: boolean }) {
+export function Permission(options: { name: string, identify: string, action: 'create' | 'delete' | 'update' | 'find' }) {
     return (target: any, propertyKey: string) => {
         Reflect.defineMetadata(PERMISSION_DEFINITION, options, target, propertyKey);
     };

b214d4ca223f5dd8499905c2a835d78f89e3bd90
dzzzzzy <insistyao_0522@163.com>
2018-08-15 17:24:44 +0800

    chore(*): remove unused files

--- test/user-module/decorators/resource.decorator.test.ts
+++
@@ -1,9 +0,0 @@
-import 'reflect-metadata';
-
-export const RESOURCE_DEFINITION = 'user:resource_definition';
-
-export function Resource(options: { name: string, identify: string }) {
-    return (target: any) => {
-        Reflect.defineMetadata(RESOURCE_DEFINITION, options, target);
-    };
-}

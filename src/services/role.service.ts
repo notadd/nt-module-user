a0e85967dd2549a0d65ff3cc11bb1736c9709eb8
dzzzzzy <insistyao_0522@163.com>
2018-08-27 19:58:36 +0800

    refactor(service): refactor role service logic

--- src/services/role.service.ts
+++ src/services/role.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
@@ -6,10 +6,12 @@ import { InfoItem } from '../entities/info-item.entity';
 import { Permission } from '../entities/permission.entity';
 import { Role } from '../entities/role.entity';
 import { RoleInfoData } from '../interfaces/role.interface';
+import { EntityCheckService } from './entity-check.service';
 
 @Injectable()
 export class RoleService {
     constructor(
+        @Inject(EntityCheckService) private readonly entityCheckService: EntityCheckService,
         @InjectRepository(Role) private readonly roleRepo: Repository<Role>,
         @InjectRepository(Permission) private readonly permissionRepo: Repository<Permission>
     ) { }
@@ -19,16 +21,8 @@ export class RoleService {
      * @param name
      */
     async createRole(name: string) {
-        const exist = await this.roleRepo.findOne({ name });
-        if (exist) {
-            new HttpException(`name为${name}的角色已存在`, 402);
-        }
-        const role = this.roleRepo.create({ name });
-        try {
-            await this.roleRepo.save(role);
-        } catch (error) {
-            throw new HttpException(`数据库错误：${error.toString()}`, 401);
-        }
+        await this.entityCheckService.checkNameExist(Role, name);
+        await this.roleRepo.save(this.roleRepo.create({ name }));
     }
 
     /**
@@ -39,22 +33,15 @@ export class RoleService {
     async updateRole(id: number, name: string) {
         const role = await this.roleRepo.findOne(id);
         if (!role) {
-            new HttpException(`id为${id}的角色不存在`, 404);
+            throw new HttpException(`id为${id}的角色不存在`, 404);
         }
 
         if (name !== role.name) {
-            const exist = await this.roleRepo.findOne({ name });
-            if (exist) {
-                new HttpException(`name为${name}的角色已存在`, 402);
-            }
+            await this.entityCheckService.checkNameExist(Role, name);
         }
 
         role.name = name;
-        try {
-            await this.roleRepo.save(role);
-        } catch (error) {
-            throw new HttpException(`数据库错误：${error.toString()}`, 401);
-        }
+        await this.roleRepo.save(role);
     }
 
     /**
@@ -64,14 +51,14 @@ export class RoleService {
     async deleteRole(id: number) {
         const role = await this.roleRepo.findOne(id, { relations: ['permissions'] });
         if (!role) {
-            new HttpException(`id为${id}的角色不存在`, 404);
+            throw new HttpException(`id为${id}的角色不存在`, 404);
         }
 
         try {
             this.roleRepo.createQueryBuilder('role').relation(Role, 'permissions').of(role).remove(role.permissions);
             await this.roleRepo.remove(role);
         } catch (err) {
-            throw new HttpException(`数据库错误：${err.toString()}`, 401);
+            throw new HttpException(`数据库错误：${err.toString()}`, 500);
         }
     }
 
@@ -83,7 +70,7 @@ export class RoleService {
     async setPermissions(id: number, permissionIds: number[]) {
         const role = await this.roleRepo.findOne(id);
         if (!role) {
-            new HttpException(`id为${id}的角色不存在`, 404);
+            throw new HttpException(`id为${id}的角色不存在`, 404);
         }
 
         const permissionArr = await this.permissionRepo.findByIds(permissionIds);
@@ -98,11 +85,7 @@ export class RoleService {
         });
 
         role.permissions = permissionArr;
-        try {
-            await this.roleRepo.save(role);
-        } catch (error) {
-            throw new HttpException(`数据库错误：${error.toString()}`, 401);
-        }
+        await this.roleRepo.save(role);
     }
 
     /**
@@ -118,26 +101,37 @@ export class RoleService {
      */
     async findOneRoleInfo(roleId: number) {
         const role = await this.roleRepo.findOne(roleId, { relations: ['permissions'] });
-        let roleInfoData: RoleInfoData;
 
-        if (!role) return roleInfoData;
+        if (!role) {
+            throw new HttpException('指定角色不存在', 404);
+        }
 
-        roleInfoData = {
+        const roleInfoData: RoleInfoData = {
             id: role.id,
             name: role.name,
             permissions: (role.permissions && role.permissions.length > 0) ? role.permissions : [],
-            infoItems: await this.findInfoGroupItemsByIds([roleId])
+            infoItems: await this.findInfoGroupItemsByIds([roleId], true, true)
         };
+        return roleInfoData;
     }
 
     /**
      * 查询角色所属信息组下的所有信息项
      *
      * @param ids 角色ID数组
+     * @param registerDisplay 是否是注册页数据
+     * @param informationDisplay 是否是资料页数据
      */
-    async findInfoGroupItemsByIds(ids: number[]) {
+    async findInfoGroupItemsByIds(ids: number[], registerDisplay: boolean, informationDisplay: boolean) {
         let infoItemsArr: InfoItem[] = [];
-        const roles = await this.roleRepo.findByIds(ids, { relations: ['infoGroup', 'infoGroup.infoItem'] });
+        const roles = await this.roleRepo.createQueryBuilder('role')
+            .leftJoinAndSelect('infoGroup', 'infoGroup')
+            .leftJoinAndSelect('infoGroup.infoItems', 'infoItems')
+            .whereInIds(ids)
+            .andWhere('infoItems.registerDisplay = :registerDisplay', { registerDisplay })
+            .andWhere('infoItems.informationDisplay = :informationDisplay', { informationDisplay })
+            .orderBy('infoItem.order', 'ASC')
+            .getMany();
 
         if (!roles || roles.length === 0) {
             return infoItemsArr;

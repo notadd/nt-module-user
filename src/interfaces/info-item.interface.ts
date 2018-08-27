f71eb43943dbf08220c34ac806557fddd3b1b31c
dzzzzzy <insistyao_0522@163.com>
2018-08-27 19:57:25 +0800

    feat(interface): add update info item input interface

---
+++ src/interfaces/info-item.interface.ts
@@ -0,0 +1,9 @@
+export interface UpdateInfoItemInput {
+    id: number;
+    order: number;
+    type: string;
+    name: string;
+    description: string;
+    registerDisplay: boolean;
+    informationDisplay: boolean;
+}

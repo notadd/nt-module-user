e8c97444086e802652dafbe466589c601f502204
dzzzzzy <insistyao_0522@163.com>
2018-09-07 17:52:24 +0800

    feat(auth): add auth service i18n support

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { AuthenticationError } from 'apollo-server-core';
+import { __ as t } from 'i18n';
 import * as jwt from 'jsonwebtoken';
 
 import { User } from '../entities/user.entity';
@@ -27,13 +28,13 @@ export class AuthService {
 
         let token = req.headers.authorization as string;
         if (!token) {
-            throw new AuthenticationError(`Request header lacks authorization parameters，it should be: 'Authorization' or 'authorization'`);
+            throw new AuthenticationError(t('Request header lacks authorization parameters，it should be: Authorization or authorization'));
         }
 
         if (['Bearer ', 'bearer '].includes(token.slice(0, 7))) {
             token = token.slice(7);
         } else {
-            throw new AuthenticationError(`The authorization code prefix is incorrect. it should be: 'Bearer ' or 'bearer '`);
+            throw new AuthenticationError(t('The authorization code prefix is incorrect. it should be: Bearer or bearer'));
         }
 
         try {
@@ -41,10 +42,10 @@ export class AuthService {
             return this.userService.findOneWithRolesAndPermissions(decodedToken.username);
         } catch (error) {
             if (error instanceof jwt.JsonWebTokenError) {
-                throw new AuthenticationError('The authorization code is incorrect');
+                throw new AuthenticationError(t('The authorization code is incorrect'));
             }
             if (error instanceof jwt.TokenExpiredError) {
-                throw new AuthenticationError('The authorization code has expired');
+                throw new AuthenticationError(t('The authorization code has expired'));
             }
         }
     }

23de3ddc7af6b9f70dfa19566dfe08f3b8fdef86
dzzzzzy <insistyao_0522@163.com>
2018-09-19 15:03:15 +0800

    perf(user): improve performance

--- src/user.module.ts
+++ src/user.module.ts
@@ -3,6 +3,7 @@ import { APP_GUARD } from '@nestjs/core';
 import { ModulesContainer } from '@nestjs/core/injector/modules-container';
 import { MetadataScanner } from '@nestjs/core/metadata-scanner';
 import { InjectRepository, TypeOrmModule } from '@nestjs/typeorm';
+import { SmsModule } from '@notadd/addon-sms';
 import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
 import { __ as t, configure as i18nConfigure } from 'i18n';
 import { join } from 'path';
@@ -17,6 +18,7 @@ import { Organization } from './entities/organization.entity';
 import { Permission } from './entities/permission.entity';
 import { Resource } from './entities/resource.entity';
 import { Role } from './entities/role.entity';
+import { SystemModule } from './entities/system-module.entity';
 import { UserInfo } from './entities/user-info.entity';
 import { User } from './entities/user.entity';
 import { InfoGroupResolver } from './resolvers/info-group.resolver';
@@ -24,6 +26,7 @@ import { InfoItemResolver } from './resolvers/info-item.resolver';
 import { OrganizationResolver } from './resolvers/organization.resolver';
 import { ResourceResolver } from './resolvers/resource.resolver';
 import { RoleResolver } from './resolvers/role.resolver';
+import { SystemModuleResolver } from './resolvers/system-module.resolver';
 import { UserResolver } from './resolvers/user.resolver';
 import { EntityCheckService } from './services/entity-check.service';
 import { InfoGroupService } from './services/info-group.service';
@@ -31,13 +34,15 @@ import { InfoItemService } from './services/info-item.service';
 import { OrganizationService } from './services/organization.service';
 import { ResourceService } from './services/resource.service';
 import { RoleService } from './services/role.service';
+import { SystemModuleService } from './services/system-module.service';
 import { UserService } from './services/user.service';
 import { CryptoUtil } from './utils/crypto.util';
 
 @Global()
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Organization, User, Role, Resource, Permission, InfoGroup, InfoItem, UserInfo])
+        TypeOrmModule.forFeature([Organization, User, Role, SystemModule, Resource, Permission, InfoGroup, InfoItem, UserInfo]),
+        SmsModule
     ],
     controllers: [],
     providers: [
@@ -47,6 +52,7 @@ import { CryptoUtil } from './utils/crypto.util';
         OrganizationResolver, OrganizationService,
         UserResolver, UserService,
         RoleResolver, RoleService,
+        SystemModuleResolver, SystemModuleService,
         ResourceResolver, ResourceService,
         InfoGroupResolver, InfoGroupService,
         InfoItemResolver, InfoItemService,
@@ -60,6 +66,7 @@ export class UserModule implements OnModuleInit {
     constructor(
         @Inject(UserService) private readonly userService: UserService,
         @Inject(ModulesContainer) private readonly modulesContainer: ModulesContainer,
+        @InjectRepository(SystemModule) private readonly systemModuleRepo: Repository<SystemModule>,
         @InjectRepository(Resource) private readonly resourceRepo: Repository<Resource>,
         @InjectRepository(Permission) private readonly permissionRepo: Repository<Permission>,
         @InjectRepository(Role) private readonly roleRepo: Repository<Role>,
@@ -96,8 +103,7 @@ export class UserModule implements OnModuleInit {
      * Load resources, permission annotations, and save them to the database
      */
     private async loadResourcesAndPermissions() {
-        const metadataMap: Map<string, { resource: Resource, permissions: Permission[] }> = new Map();
-        // Iterate Modules from module container
+        const metadataMap: Map<string, Resource[]> = new Map();
         this.modulesContainer.forEach(module => {
             for (const [key, value] of [...module.components, ...module.routes]) {
                 const isResolverOrController =
@@ -106,92 +112,109 @@ export class UserModule implements OnModuleInit {
                             .includes(key)).length > 0;
 
                 if (isResolverOrController) {
-                    // Get the metadata in the @Resource() annotation on the Resolver or Controller class
                     const resource: Resource = Reflect.getMetadata(RESOURCE_DEFINITION, value.instance.constructor);
-                    // Get the prototype object of the Resolver or Controller class
                     const prototype = Object.getPrototypeOf(value.instance);
+
                     if (prototype) {
-                        // Get the method name in the Resolver or Controller class,
-                        // the name in the callback function is the method name in the current class
                         const permissions: Permission[] = this.metadataScanner.scanFromPrototype(value.instance, prototype, name => {
-                            // Get the metadata in the @Permission() annotation on the method in the Resolver or Controller class
                             return Reflect.getMetadata(PERMISSION_DEFINITION, value.instance, name);
                         });
-                        // If the metadata exists, it will be added to the resource collection,
-                        // and it will be automatically deduplicated according to resource.indetify
+
                         if (resource) {
-                            // Translate the resources name
                             resource.name = t(resource.name);
-                            // Translate the permissions name
                             permissions.forEach(permission => {
                                 permission.name = t(permission.name);
                             });
-                            metadataMap.set(resource.identify, { resource, permissions });
+
+                            if (resource.permissions) {
+                                resource.permissions.push(...permissions);
+                            } else {
+                                resource.permissions = permissions;
+                            }
+
+                            if (metadataMap.has(module.metatype.name)) {
+                                metadataMap.get(module.metatype.name).push(resource);
+                            } else {
+                                metadataMap.set(module.metatype.name, [resource]);
+                            }
                         }
                     }
                 }
             }
         });
 
-        /**
-         * LOOK ME:
-         *
-         * The following are the create and delete logic for resources and permissions.
-         *
-         * When the permission uniquely identifies the change,
-         * the relationship between the changed permission and the corresponding role will also be deleted.
-         *
-         */
-
-        // All resource annotations and all permission annotations that were scanned
-        const scannedResourcesAndPermissions = [...metadataMap.values()].map(metadataValue => {
-            // Bind permissions to the corresponding resource
-            metadataValue.permissions.forEach(v => v.resource = metadataValue.resource);
-            return { permissions: metadataValue.permissions, resource: metadataValue.resource };
+        // Sacnned modules
+        const scannedModuleNames = [...metadataMap.keys()].map(v => t(v));
+        // Delete removed module
+        const notExistingModule = await this.systemModuleRepo.find({
+            where: { name: Not(In(scannedModuleNames.length ? scannedModuleNames : ['all'])) }
         });
+        if (notExistingModule.length) await this.systemModuleRepo.delete(notExistingModule.map(v => v.id));
+        // Create new module
+        const existingModules = await this.systemModuleRepo.find({ order: { id: 'ASC' } });
+        const newModules = scannedModuleNames.filter(sm => !existingModules.map(v => v.name).includes(sm)).map(moduleName => {
+            return this.systemModuleRepo.create({ name: moduleName });
+        });
+        if (newModules.length) await this.systemModuleRepo.save(newModules);
+        // Update existing module
+        if (existingModules.length) {
+            existingModules.forEach(em => {
+                em.name = scannedModuleNames.find(sm => sm === em.name);
+            });
+            await this.systemModuleRepo.save(existingModules);
+        }
 
-        // All resource annotations that were scanned
-        const scannedResources = scannedResourcesAndPermissions.map(v => v.resource);
-
-        // Remove the resources and their permissions which were removed from the annotation
-        const resourceIdentifies = [...metadataMap.keys()].length === 0 ? ['__delete_all_resource__'] : [...metadataMap.keys()];
+        // Sacnned resources
+        for (const [key, value] of metadataMap) {
+            const resourceModule = await this.systemModuleRepo.findOne({ where: { name: t(key) } });
+            value.forEach(async resouece => {
+                resouece.systemModule = resourceModule;
+            });
+        }
+        const scannedResources: Resource[] = <Resource[]>[].concat(...metadataMap.values());
+        // Delete removed resource
+        const resourceIdentifies = scannedResources.length ? scannedResources.map(v => v.identify) : ['__delete_all_resource__'];
         const notExistResources = await this.resourceRepo.find({ where: { identify: Not(In(resourceIdentifies)) } });
         if (notExistResources.length > 0) await this.resourceRepo.delete(notExistResources.map(v => v.id));
-        // Filter out the new resources
+        // Create new resource
         const existResources = await this.resourceRepo.find({ order: { id: 'ASC' } });
         const newResourcess = scannedResources.filter(sr => !existResources.map(v => v.identify).includes(sr.identify));
-        // Save the new resources
         if (newResourcess.length > 0) await this.resourceRepo.save(this.resourceRepo.create(newResourcess));
-        // Update existing resources name
-        existResources.forEach(er => {
-            er.name = scannedResources.find(sr => sr.identify === er.identify).name;
-        });
-        await this.resourceRepo.save(existResources);
+        // Update resource
+        if (existResources.length) {
+            existResources.forEach(er => {
+                er.name = scannedResources.find(sr => sr.identify === er.identify).name;
+            });
+            await this.resourceRepo.save(existResources);
+        }
 
-        // All permission annotations that were scanned
-        const scannedPermissions = <Permission[]>[].concat(...scannedResourcesAndPermissions.map(v => v.permissions));
-        // Query the resources of all the permission annotations scanned
+        // Sacnned permissions
+        const scannedPermissions = <Permission[]>[].concat(...scannedResources.map(metadataValue => {
+            metadataValue.permissions.forEach(v => v.resource = metadataValue);
+            return metadataValue.permissions;
+        }));
+        // Delete removed permission
         const resource = await this.resourceRepo.find({ where: { identify: In(scannedPermissions.map(v => v.resource.identify)) } });
-        // Bind resources to permissions
         scannedPermissions.forEach(permission => {
             permission.resource = resource.find(v => v.identify === permission.resource.identify);
         });
-        // Remove the permissions that were removed from annotations
+        // Create removed permission
         // tslint:disable-next-line:max-line-length
-        const permissionIdentifies = scannedPermissions.map(v => v.identify).length === 0 ? ['__delete_all_permission__'] : scannedPermissions.map(v => v.identify);
+        const permissionIdentifies = scannedPermissions.length ? scannedPermissions.map(v => v.identify) : ['__delete_all_permission__'];
         const notExistPermissions = await this.permissionRepo.find({ where: { identify: Not(In(permissionIdentifies)) } });
         if (notExistPermissions.length > 0) await this.permissionRepo.delete(notExistPermissions.map(v => v.id));
-        // Filter out the new permissions
+
         const existPermissions = await this.permissionRepo.find({ order: { id: 'ASC' } });
         const newPermissions = scannedPermissions.filter(sp => !existPermissions.map(v => v.identify).includes(sp.identify));
-        // Save the new permissions
         if (newPermissions.length > 0) await this.permissionRepo.save(this.permissionRepo.create(newPermissions));
-        // Update existing permissions name
-        existPermissions.forEach(ep => {
-            ep.name = scannedPermissions.find(sp => sp.identify === ep.identify).name;
-            ep.action = scannedPermissions.find(sp => sp.identify === ep.identify).action;
-        });
-        await this.permissionRepo.save(existPermissions);
+        // Update permission
+        if (existPermissions.length) {
+            existPermissions.forEach(ep => {
+                ep.name = scannedPermissions.find(sp => sp.identify === ep.identify).name;
+                ep.action = scannedPermissions.find(sp => sp.identify === ep.identify).action;
+            });
+            await this.permissionRepo.save(existPermissions);
+        }
     }
 
     /**

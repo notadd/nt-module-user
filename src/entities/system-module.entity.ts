c279d697b2d35b84fc7c413fea572acbefe9b970
dzzzzzy <insistyao_0522@163.com>
2018-09-20 15:35:41 +0800

    fix(system-module): fix system module id bug

--- src/entities/system-module.entity.ts
+++ src/entities/system-module.entity.ts
@@ -1,11 +1,11 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
 
 import { Resource } from './resource.entity';
 
 @Entity('system_module')
 export class SystemModule {
-    @PrimaryGeneratedColumn()
-    id: number;
+    @PrimaryColumn()
+    id: string;
 
     @Column()
     name: string;
--- src/user.module.ts
+++ src/user.module.ts
@@ -110,75 +110,74 @@ export class UserModule implements OnModuleInit {
      * Load resources, permission annotations, and save them to the database
      */
     private async loadResourcesAndPermissions() {
-        const metadataMap: Map<string, Resource[]> = new Map();
-        this.modulesContainer.forEach(module => {
-            for (const [key, value] of [...module.components, ...module.routes]) {
+        const metadataMap: Map<string, { name: string, resource: Resource[] }> = new Map();
+        this.modulesContainer.forEach((moduleValue, moduleKey) => {
+            for (const [componentKey, componentKeyValue] of [...moduleValue.components, ...moduleValue.routes]) {
                 const isResolverOrController =
-                    Reflect.getMetadataKeys(value.instance.constructor)
+                    Reflect.getMetadataKeys(componentKeyValue.instance.constructor)
                         .filter(key => ['graphql:resolver_type', 'path']
                             .includes(key)).length > 0;
 
                 if (isResolverOrController) {
-                    const resource: Resource = Reflect.getMetadata(RESOURCE_DEFINITION, value.instance.constructor);
-                    const prototype = Object.getPrototypeOf(value.instance);
+                    const resource: Resource = Reflect.getMetadata(RESOURCE_DEFINITION, componentKeyValue.instance.constructor);
+                    const prototype = Object.getPrototypeOf(componentKeyValue.instance);
 
-                    if (prototype) {
-                        const permissions: Permission[] = this.metadataScanner.scanFromPrototype(value.instance, prototype, name => {
-                            return Reflect.getMetadata(PERMISSION_DEFINITION, value.instance, name);
+                    if (resource && prototype) {
+                        const permissions: Permission[] = this.metadataScanner.scanFromPrototype(componentKeyValue.instance, prototype, name => {
+                            return Reflect.getMetadata(PERMISSION_DEFINITION, componentKeyValue.instance, name);
                         });
+                        resource.permissions = permissions;
 
-                        if (resource) {
-                            resource.name = t(resource.name);
-                            permissions.forEach(permission => {
-                                permission.name = t(permission.name);
-                            });
-
-                            if (resource.permissions) {
-                                resource.permissions.push(...permissions);
-                            } else {
-                                resource.permissions = permissions;
-                            }
-
-                            if (metadataMap.has(module.metatype.name)) {
-                                metadataMap.get(module.metatype.name).push(resource);
-                            } else {
-                                metadataMap.set(module.metatype.name, [resource]);
-                            }
+                        if (metadataMap.has(moduleKey)) {
+                            metadataMap.get(moduleKey).name = t(moduleValue.metatype.name);
+                            metadataMap.get(moduleKey).resource.push(resource);
+                        } else {
+                            metadataMap.set(moduleKey, { name: t(moduleValue.metatype.name), resource: [resource] });
                         }
                     }
                 }
             }
         });
 
+        metadataMap.forEach(value => {
+            value.resource.forEach(resource => {
+                resource.name = t(resource.name);
+                resource.permissions.forEach(p => p.name = t(p.name));
+            });
+        });
+
         // Sacnned modules
-        const scannedModuleNames = [...metadataMap.keys()].map(v => t(v));
+        const scannedModules: { id: string, name: string }[] = [];
+        metadataMap.forEach((v, k) => {
+            scannedModules.push({ id: k, name: v.name });
+        });
+
         // Delete removed module
         const notExistingModule = await this.systemModuleRepo.find({
-            where: { name: Not(In(scannedModuleNames.length ? scannedModuleNames : ['all'])) }
+            where: { id: Not(In(scannedModules.length ? scannedModules.map(v => v.id) : ['all'])) }
         });
         if (notExistingModule.length) await this.systemModuleRepo.delete(notExistingModule.map(v => v.id));
         // Create new module
         const existingModules = await this.systemModuleRepo.find({ order: { id: 'ASC' } });
-        const newModules = scannedModuleNames.filter(sm => !existingModules.map(v => v.name).includes(sm)).map(moduleName => {
-            return this.systemModuleRepo.create({ name: moduleName });
-        });
-        if (newModules.length) await this.systemModuleRepo.save(newModules);
+        const newModules = scannedModules.filter(sm => !existingModules.map(v => v.id).includes(sm.id));
+        if (newModules.length) await this.systemModuleRepo.save(this.systemModuleRepo.create(newModules));
         // Update existing module
         if (existingModules.length) {
             existingModules.forEach(em => {
-                em.name = scannedModuleNames.find(sm => sm === em.name);
+                em.name = scannedModules.find(sm => sm.id === em.id).name;
             });
             await this.systemModuleRepo.save(existingModules);
         }
 
         // Sacnned resources
         for (const [key, value] of metadataMap) {
-            const resourceModule = await this.systemModuleRepo.findOne({ where: { name: t(key) } });
-            value.forEach(async resouece => {
+            const resourceModule = await this.systemModuleRepo.findOne({ where: { id: key } });
+            value.resource.forEach(async resouece => {
                 resouece.systemModule = resourceModule;
             });
         }
-        const scannedResources: Resource[] = <Resource[]>[].concat(...metadataMap.values());
+        const scannedResources: Resource[] = <Resource[]>[].concat(...[...metadataMap.values()].map(v => v.resource));
+
         // Delete removed resource
         const resourceIdentifies = scannedResources.length ? scannedResources.map(v => v.identify) : ['__delete_all_resource__'];
         const notExistResources = await this.resourceRepo.find({ where: { identify: Not(In(resourceIdentifies)) } });
@@ -206,7 +205,6 @@ export class UserModule implements OnModuleInit {
             permission.resource = resource.find(v => v.identify === permission.resource.identify);
         });
         // Create removed permission
-        // tslint:disable-next-line:max-line-length
         const permissionIdentifies = scannedPermissions.length ? scannedPermissions.map(v => v.identify) : ['__delete_all_permission__'];
         const notExistPermissions = await this.permissionRepo.find({ where: { identify: Not(In(permissionIdentifies)) } });
         if (notExistPermissions.length > 0) await this.permissionRepo.delete(notExistPermissions.map(v => v.id));
